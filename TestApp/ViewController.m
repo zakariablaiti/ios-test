53dddad558bb0be3566e004eecc3e483f6f710b0
bluesettle <bluesettle@bluesettles-Mac.local>
2013-04-04 10:51:13 +0000

    2 commit test

--- TestApp/ViewController.m
+++ TestApp/ViewController.m
@@ -19,6 +19,7 @@
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
     // first comit test
+    // second for github online
 }
 
 - (void)didReceiveMemoryWarning
